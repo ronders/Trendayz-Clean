cf532e5d3d4ae77b293243ca95c9893af6bb2a93
Ron Dyrcz <ron@port80corp.com>
2015-01-27 19:54:53 -0600

    Site framework
    Install and config of Opencart, VQmod, Pav theme and modules

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:7888/Trendays/admin/');
+define('HTTP_CATALOG', 'http://localhost:7888/Trendays/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:7888/Trendays/admin/');
+define('HTTPS_CATALOG', 'http://localhost:7888/Trendays/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Ronders/Projects/Trendays/admin/');
+define('DIR_SYSTEM', '/Users/Ronders/Projects/Trendays/system/');
+define('DIR_DATABASE', '/Users/Ronders/Projects/Trendays/system/database/');
+define('DIR_LANGUAGE', '/Users/Ronders/Projects/Trendays/admin/language/');
+define('DIR_TEMPLATE', '/Users/Ronders/Projects/Trendays/admin/view/template/');
+define('DIR_CONFIG', '/Users/Ronders/Projects/Trendays/system/config/');
+define('DIR_IMAGE', '/Users/Ronders/Projects/Trendays/image/');
+define('DIR_CACHE', '/Users/Ronders/Projects/Trendays/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Ronders/Projects/Trendays/download/');
+define('DIR_LOGS', '/Users/Ronders/Projects/Trendays/system/logs/');
+define('DIR_CATALOG', '/Users/Ronders/Projects/Trendays/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.dev.trendayz.com');
+define('DB_USERNAME', 'adyrcz');
+define('DB_PASSWORD', 'Achi11e5');
+define('DB_DATABASE', 'db_trendayz_dev');
+define('DB_PREFIX', 'tz4_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:7888/Trendays/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:7888/Trendays/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Ronders/Projects/Trendays/catalog/');
+define('DIR_SYSTEM', '/Users/Ronders/Projects/Trendays/system/');
+define('DIR_DATABASE', '/Users/Ronders/Projects/Trendays/system/database/');
+define('DIR_LANGUAGE', '/Users/Ronders/Projects/Trendays/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Ronders/Projects/Trendays/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Ronders/Projects/Trendays/system/config/');
+define('DIR_IMAGE', '/Users/Ronders/Projects/Trendays/image/');
+define('DIR_CACHE', '/Users/Ronders/Projects/Trendays/system/cache/');
+define('DIR_DOWNLOAD', '/Users/Ronders/Projects/Trendays/download/');
+define('DIR_LOGS', '/Users/Ronders/Projects/Trendays/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.dev.trendayz.com');
+define('DB_USERNAME', 'adyrcz');
+define('DB_PASSWORD', 'Achi11e5');
+define('DB_DATABASE', 'db_trendayz_dev');
+define('DB_PREFIX', 'tz4_');
+?>
